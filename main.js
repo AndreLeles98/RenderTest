deb0190ebee769ed419adf103ce64f8bd22f62a4
AndreDev <66388519+AndreLeles98@users.noreply.github.com>
2023-11-14 08:46:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+var lastUpdate = Date.now();
+var myInterval = setInterval(tick, 0);
+const loader = new THREE.GLTFLoader();
+var scene;
+var camera;
+var globalLight;
+var renderer;
+
+function start(){
+	scene = THREE.Scene();
+	scene.background = new THREE.Color(0xFFFF00);
+	
+	camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+	camera.rotation.y = 45/180*Math.PI;
+	camera.position.x = 800;
+	
+	
+	globalLight = new THREE.AmbientLight(0xffffff, 100);
+	
+	renderer = new THREE.WebGLRenderer({antialias:true})
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+	
+	loader.load("helpCube.gltf", function(gltf){
+		scene.add(gltf.scene);
+		renderer.render(scene, camera);
+	});
+}
+
+function update() {
+	
+}
+
+
+function render() {
+	
+}
+
+
+function tick() {
+    var now = Date.now();
+    var dt = now - lastUpdate;
+    lastUpdate = now;
+    update(dt);
+    render(dt);
+}
+
+start();
+//while (true){
+	//tick();
+//}
